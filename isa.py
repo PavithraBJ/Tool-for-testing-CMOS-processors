5f89822c20a79bc9c0531b842587a976be1c5d2a
PavithraBJ <pavithrajaganathan@yahoo.com>
2016-06-21 15:47:55 -0400

    isa.py
    Machine description file which includes the set of instructions supported by the test tool. Instructions are classified based on the PISA ISA encoding.

---
+++ isa.py
@@ -0,0 +1,11 @@
+class1 = ['ADD','ADDU','SUB','SUBU','AND','NOR','OR','XOR','SLT','SLTU','SLLV','SRAV','SRLV','MUL']
+class2 = ['ADDI','ADDIU','ANDI','ORI','XORI','SLTI','SLTIU']
+class3 = ['LI','LUI']
+class4 = ['MOVE','DIV','DIVU','MULT','MULTU']
+class5 = ['SLL','SRA','SRL']
+class6 = ['MFHI','MFLO','MTHI','MTLO']
+class7 = ['BEQ','BGEZ','BGTZ','BLEZ','BLTZ','BNE']
+registers = ['$8','$9','$10','$11','$12']
+hexadigit = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+
+
